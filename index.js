24a06f5d459e662674257cd0abb282bbc64d55b9
uday thammeneni <uday.thammeneni@xcubelabs.com>
2026-07-04 20:08:28 +0530

    staging deploy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Health Check
 app.get('/', (req, res) => {
   res.json({
-    message: 'Node.js server version 2 is running successfully!',
+    message: 'Node.js server version 2 - staging is running successfully!',
     environment: process.env.APP_ENV || 'development',
     timestamp: new Date().toISOString(),
   });
