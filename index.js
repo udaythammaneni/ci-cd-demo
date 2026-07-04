e543b16e52149e8baed4653015a079aa8e176741
uday thammeneni <uday.thammeneni@xcubelabs.com>
2026-07-04 11:34:04 +0530

    api changes added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('App running inside CI');
 // const name = 'Demo';
 console.log('Environment:', process.env.APP_ENV);
 console.log('API_URL', process.env.API_URL);
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,24 +10,24 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Health Check
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    message: "Node.js server is running 🚀",
-    environment: process.env.APP_ENV || "development",
+    message: 'Node.js server is running 🚀',
+    environment: process.env.APP_ENV || 'development',
     timestamp: new Date().toISOString(),
   });
 });
 
 // Sample API
-app.get("/api/hello", (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({
     success: true,
-    message: "Hello from Node.js API!",
+    message: 'Hello from Node.js API!',
   });
 });
 
 // POST API
-app.post("/api/message", (req, res) => {
+app.post('/api/message', (req, res) => {
   res.json({
     success: true,
     received: req.body,
